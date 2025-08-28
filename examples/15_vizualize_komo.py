e607069f830c18e6b8946eba27aa3a46c834f838
Pierre Squarra <pierre@squarra.de>
2025-08-28 16:32:10 +0200

    refined the manipulation primitvevs and sim

--- examples/15_vizualize_komo.py
+++ examples/15_vizualize_komo.py
@@ -1,16 +1,26 @@
-from robotic._robotic import FS, JT, OT, ST
+from robotic._robotic import JT, ST
 from robotic.manipulation import Manipulation
 from robotic.scenario import PandaScenario
 
 config = PandaScenario()
 box_size = [0.15, 0.06, 0.06, 0.005]
 box_z = (config.getFrame("table").getSize()[2] / 2) + (box_size[2] / 2)
-box = config.addFrame("box1", "table").setJoint(JT.rigid).setShape(ST.ssBox, box_size).setRelativePosition([0.4, 0.4, box_z]).setContact(1)
+box = (
+    config.addFrame("box1", "table")
+    .setJoint(JT.rigid)
+    .setShape(ST.ssBox, box_size)
+    .setRelativePosition([0, 0.4, box_z])
+    .setContact(1)
+    .setMass(0.1)
+    .setAttributes({"friction": 0.5})
+)
 
-for primitive in Manipulation.primitives:
-    man = Manipulation(config, box.name, slices=20)
-    getattr(man, primitive)()
-    # man.target_pos(box.getRelativePosition() + [0.1, 0.1, 0.0])
-    ret = man.solve()
-    man.view(pause=True, txt=f"{primitive}={ret.feasible}")
-    man.simulate()
+man = Manipulation(config, box.name, slices=20)
+man.grasp_obj_y()
+target_pos = box.getRelativePosition() + [0.1, 0.0, 0.0]
+man.target_pos_up_z(target_pos)
+ret = man.solve()
+man.view(pause=True, txt=str(ret.feasible))
+if ret.feasible:
+    man.simulate(view=False)
+    print(man.config.getFrame("box1").getRelativePosition())
--- robotic/manipulation.py
+++ robotic/manipulation.py
@@ -4,7 +4,7 @@ import numpy as np
 
 from robotic import FS, JT, KOMO, OT, ControlMode, NLP_Solver, Simulation, SimulationEngine
 from robotic.helpers import DEBUG
-from robotic.scenario import Scenario
+from robotic.scenario import PandaScenario
 
 gripper = "gripper"
 table = "table"
@@ -23,12 +23,14 @@ class Manipulation:
         "grasp_obj_z",
     ]
 
-    def __init__(self, scenario: Scenario, obj: str, slices=1):
+    def __init__(self, scenario: PandaScenario, obj: str, slices=1):
+        self.scenario = scenario
         self.komo = KOMO(scenario, 2.0, slices, 1, True)
         self.komo.addControlObjective([], 0, 1e-2)
         self.komo.addControlObjective([], 1, 1e-1)
         self.komo.addObjective([], FS.jointLimits, [], OT.ineq, [1e0])
         self.komo.addObjective([], FS.accumulatedCollisions, [], OT.eq, [1e0])
+        self.komo.addQuaternionNorms()
 
         self.obj = obj
         self.slices = slices
@@ -89,12 +91,13 @@ class Manipulation:
 
         # gripper position
         target = 0.5 * self.get_bbox(self.obj) - 0.02
-        self.komo.addObjective([1.0], FS.positionRel, [gripper, self.obj], OT.eq, scale=x_axis * 1e1)
+        self.komo.addObjective([0.8, 1.0], FS.positionRel, [gripper, self.obj], OT.eq, scale=x_axis * 1e1)
         self.komo.addObjective([1.0], FS.positionRel, [gripper, self.obj], OT.ineq, scale=yz_plane * 1e1, target=[target])
         self.komo.addObjective([1.0], FS.positionRel, [gripper, self.obj], OT.ineq, scale=yz_plane * (-1e1), target=[-target])
+        self.komo.addObjective([1.0], FS.positionRel, [gripper, self.obj], OT.eq, scale=[1e-1])
         # gripper orientation
-        self.komo.addObjective([0.8, 1.0], align[0], [gripper, self.obj], OT.eq, [1e0])
-        self.komo.addObjective([0.8, 1.0], align[1], [gripper, self.obj], OT.eq, [1e0])
+        self.komo.addObjective([0.8, 1.0], align[0], [gripper, self.obj], OT.eq, scale=[1e0], target=[0])
+        self.komo.addObjective([0.8, 1.0], align[1], [gripper, self.obj], OT.eq, scale=[1e0], target=[0])
 
     def grasp_obj_x(self):
         self._grasp_obj(0, [FS.scalarProductXY, FS.scalarProductXZ])
@@ -105,10 +108,34 @@ class Manipulation:
     def grasp_obj_z(self):
         self._grasp_obj(2, [FS.scalarProductXX, FS.scalarProductXY])
 
+    def target_pos_up_axis(self, dim: int, dir: int, align: FS, pos: np.typing.ArrayLike):
+        """Places the object on top of the table at a certain position with the specified up axis"""
+        self.komo.addObjective([2.0], align, [self.obj], OT.eq, [1e1], target=[0, 0, dir])
+        pos[2] = (self.get_bbox(self.obj)[dim] / 2.0) + (self.scenario.table.getSize()[2] / 2.0)
+        self.target_pos(pos)
+
+    def target_pos_up_x(self, pos: np.typing.ArrayLike):
+        self.target_pos_up_axis(0, 1, FS.vectorX, pos)
+
+    def target_pos_up_x_neg(self, pos: np.typing.ArrayLike):
+        self.target_pos_up_axis(0, -1, FS.vectorX, pos)
+
+    def target_pos_up_y(self, pos: np.typing.ArrayLike):
+        self.target_pos_up_axis(1, 1, FS.vectorY, pos)
+
+    def target_pos_up_y_neg(self, pos: np.typing.ArrayLike):
+        self.target_pos_up_axis(1, -1, FS.vectorY, pos)
+
+    def target_pos_up_z(self, pos: np.typing.ArrayLike):
+        self.target_pos_up_axis(2, 1, FS.vectorZ, pos)
+
+    def target_pos_up_z_neg(self, pos: np.typing.ArrayLike):
+        self.target_pos_up_axis(2, -1, FS.vectorZ, pos)
+
     def target_pos(self, pos: np.typing.ArrayLike):
         self.komo.addObjective([2.0], FS.positionRel, [self.obj, table], OT.eq, [1e1], target=pos)
 
-    def target_ori(self, ori: np.typing.ArrayLike):
+    def target_quat(self, ori: np.typing.ArrayLike):
         self.komo.addObjective([2.0], FS.quaternionRel, [self.obj, table], OT.eq, scale=[1e1], target=ori)
 
     def solve(self):
@@ -134,7 +161,7 @@ class Manipulation:
 
         return np.max(vertices, axis=0) - np.min(vertices, axis=0)
 
-    def simulate(self, times=1.0, tau=5e-3):
+    def simulate(self, view=True, times=2.0, tau=5e-3):
         sim = Simulation(self.config, SimulationEngine.physx, verbose=DEBUG.value)
         sim_steps = int(times // tau)
         splits = np.split(self.komo.getPath(), [self.slices])
@@ -142,17 +169,20 @@ class Manipulation:
         sim.setSplineRef(path=splits[0], times=[times])
         for _ in range(sim_steps):
             sim.step([], tau, ControlMode.spline)
-            time.sleep(tau)
-            self.config.view()
+            if view:
+                time.sleep(tau)
+                self.config.view()
 
         sim.moveGripper(gripper, 0.0)
         while not sim.gripperIsDone(gripper):
             sim.step([], tau, ControlMode.spline)
-            time.sleep(tau)
-            self.config.view()
+            if view:
+                time.sleep(tau)
+                self.config.view()
 
         sim.setSplineRef(path=splits[1], times=[1])
         for _ in range(sim_steps):
             sim.step([], tau, ControlMode.spline)
-            time.sleep(tau)
-            self.config.view()
+            if view:
+                time.sleep(tau)
+                self.config.view()
