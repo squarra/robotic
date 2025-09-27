051285feb2285e15555e8666513a70a7ca674614
Pierre Squarra <pierre@squarra.de>
2025-09-27 21:45:02 +0200

    cleaned panda visualization

--- examples/00_visualize_panda_frames.py
+++ examples/00_visualize_panda_frames.py
@@ -3,7 +3,6 @@ from robotic.scenario import PandaScenario
 config = PandaScenario()
 
 for frame in sorted(config.getFrames(), key=lambda x: x.name):
-    initial_color = frame.getMeshColors()
     frame.setColor([1, 0, 0])
-    config.view(pause=True, message=frame.name)
+    config.view(pause=True, message=f"{frame.name}={frame.asDict()}")
     frame.setColor([1, 1, 1])
