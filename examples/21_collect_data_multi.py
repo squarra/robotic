f41860480e26a9d8ef8088678c22380fa406b877
Pierre Squarra <pierre@squarra.de>
2025-09-30 22:11:01 +0200

    data collection adjusted

--- examples/21_collect_data_multi.py
+++ examples/21_collect_data_multi.py
@@ -1,85 +1,86 @@
-"""
-This example shows how to use multiprocessing when solve() is the main
-bottleneck (see 22_collect_data_timed.py). A global config object is
-updated per scene, and for each manipulation primitive a Worker is
-spawned to call solve(). Since writing to HDF5 must occur in the main
-thread, results are collected and written at the end.
-"""
-
 from multiprocessing import Pool
 
 import h5py
 import numpy as np
-import tqdm
+from tqdm import trange
 
 from robotic.manipulation import Manipulation
 from robotic.scenario import PandaScenario
 
-DATASET_PATH = "test-dataset-2.h5"
-NUM_SCENES = 1500
-START_SEED = 1000
+DATASET_PATH = "dataset.h5"
+NUM_SCENES = 100
+START_SEED = 0
 SLICES = 10  # fewer slices = faster but less accurate
-POS_OFFSET = 0.05  # move 5cm along the push/grasp axis
+INCREMENTAL_SLICES = True
 
+offset_directions = [[1, 0, 0], [-1, 0, 0], [0, 1, 0], [0, -1, 0]]
+num_offsets = len(offset_directions)
+primitives = Manipulation.primitives
+num_primitives = len(primitives)
 config = PandaScenario()
 
 
 def solve_primitive(args):
-    obj, primitive_name = args
-    man = Manipulation(config, obj, slices=SLICES)
-    frame = man.config.getFrame(obj)
-    _, primitive_dim, primitive_dir = primitive_name.split("_")
-    dim = {"x": 0, "y": 1, "z": 2}[primitive_dim]
-    dir = {"pos": 1, "neg": -1}[primitive_dir]
-    offset_local = np.zeros(3)
-    offset_local[dim] = POS_OFFSET * dir
-    offset_world = frame.getRotationMatrix() @ offset_local
-    target_pos = frame.getRelativePosition() + offset_world
-    target_pose = np.concatenate([target_pos, frame.getRelativeQuaternion()])
-    man.target_pose(target_pose)
+    oi, obj, ti, target_pose, pi = args
 
-    getattr(man, primitive_name)()
-    ret = man.solve()
-    if ret.feasible:
+    man = Manipulation(config, obj, slices=SLICES)
+    getattr(man, primitives[pi])()
+    man.target_pose(target_pose)
+    feasible = man.solve()
+    if feasible and INCREMENTAL_SLICES:
+        man = Manipulation(config, obj, slices=SLICES * 2)
+        getattr(man, primitive)()
+        man.target_pose(target_pose)
+        feasible = man.solve().feasible
+    if feasible:
         man.simulate(view=False)
-    final_pose = frame.getRelativePose()
-    return obj, primitive_name, target_pose, ret.feasible, final_pose
+
+    final_pose = man.config.getFrame(obj).getRelativePose()
+    return oi, ti, pi, feasible, final_pose
 
 
 with h5py.File(DATASET_PATH, "w") as f:
-    f.attrs["primitives"] = np.array(Manipulation.primitives, dtype=h5py.string_dtype(encoding="utf-8"))
-    for i in tqdm.trange(NUM_SCENES):
-        seed = START_SEED + i
+    f.attrs["primitives"] = np.array(primitives, dtype=h5py.string_dtype(encoding="utf-8"))
+
+    for seed in trange(START_SEED, START_SEED + NUM_SCENES, desc="Collecting data"):
         config.delete_man_frames()
-        config.add_boxes_to_scene(seed=seed)
+        config.add_boxes(seed=seed)
 
-        images, depths, seg_ids = config.compute_images_depths_and_seg_ids()
+        num_objects = len(config.man_frames)
 
-        dp_group = f.create_group(f"datapoint_{seed:04d}")
-        dp_group.attrs["seed"] = seed
-        dp_group.create_dataset("camera_positions", data=config.camera_positions)
-        dp_group.create_dataset("images", data=images, compression="gzip", chunks=True)
-        dp_group.create_dataset("depths", data=depths, compression="gzip", chunks=True)
-        dp_group.create_dataset("seg_ids", data=seg_ids, compression="gzip", chunks=True)
+        poses = np.zeros((num_objects, 7), dtype=np.float32)
+        sizes = np.zeros((num_objects, 3), dtype=np.float32)
+        target_poses = np.zeros((num_objects, num_offsets, 7), dtype=np.float32)
+        feasibles = np.zeros((num_objects, num_offsets, num_primitives), dtype=np.int8)
+        final_poses = np.zeros((num_objects, num_offsets, num_primitives, 7), dtype=np.float32)
 
-        objects_group = dp_group.create_group("objects")
         jobs = []
-        for obj in config.man_frames:
-            obj_frame = config.getFrame(obj)
-            obj_group = objects_group.create_group(obj)
-            obj_group.attrs["id"] = obj_frame.ID
-            obj_group.create_dataset("pose", data=obj_frame.getRelativePose().astype(np.float32))
-            obj_group.create_dataset("size", data=obj_frame.getSize()[:3].astype(np.float32))
+        for oi, obj in enumerate(config.man_frames):
+            frame = config.getFrame(obj)
+            poses[oi] = frame.getRelativePose()
+            sizes[oi] = frame.getSize()[:3]
 
-            obj_group.create_group("primitives")
-            for primitive in Manipulation.primitives:
-                jobs.append((obj, primitive))
+            for ti, direction in enumerate(offset_directions):
+                target_pos = config.sample_target_pos(obj, direction, seed=seed)
+                target_pose = np.concatenate([target_pos, frame.getRelativeQuaternion()])
+                target_poses[oi][ti] = target_pose
+
+                for pi, primitive in enumerate(primitives):
+                    jobs.append((oi, obj, ti, target_pose, pi))
 
         with Pool() as pool:
-            results = pool.map(solve_primitive, jobs)
+            for oi, ti, pi, feasible, final_pose in pool.map(solve_primitive, jobs):
+                feasibles[oi, ti, pi] = feasible
+                final_poses[oi, ti, pi] = final_pose
 
-        for obj, primitive_name, target_pose, feasible, final_pose in results:
-            primitive_group = objects_group[obj]["primitives"].create_group(primitive_name)
-            primitive_group.create_dataset("target_pose", data=target_pose.astype(np.float32))
-            primitive_group.create_dataset("feasible", data=feasible)
-            primitive_group.create_dataset("final_pose", data=final_pose.astype(np.float32))
+        images, depths, seg_ids = config.compute_images_depths_and_seg_ids()
+        dp = f.create_group(f"dp_{seed:04d}")
+        dp.create_dataset("cam_poses", data=config.cam_poses)
+        dp.create_dataset("images", data=images, compression="gzip", chunks=True)
+        dp.create_dataset("depths", data=depths, compression="gzip", chunks=True)
+        dp.create_dataset("seg_ids", data=seg_ids, compression="gzip", chunks=True)
+        dp.create_dataset("poses", data=poses)
+        dp.create_dataset("sizes", data=sizes)
+        dp.create_dataset("target_poses", data=target_poses)
+        dp.create_dataset("feasibles", data=feasibles)
+        dp.create_dataset("final_poses", data=final_poses)
--- examples/22_collect_data_timed.py
+++ examples/22_collect_data_timed.py
@@ -1,5 +1,4 @@
 import time
-from collections import defaultdict
 
 import h5py
 import numpy as np
@@ -8,129 +7,94 @@ from robotic.manipulation import Manipulation
 from robotic.scenario import PandaScenario
 
 DATASET_PATH = "dataset.h5"
-SLICES = 10  # fewer slices = more speed
-POS_OFFSET = 0.05  # move 5cm along the push/grasp axis
-SEED = 42
+SEED = 0
+SLICES = 10  # fewer slices = faster but less accurate
+INCREMENTAL_SLICES = True
 
-timing_data = {"solve_feasible": [], "solve_infeasible": [], "simulate": [], "per_primitive": defaultdict(list), "per_object": defaultdict(float)}
+# offset_directions = [[1, 0, 0], [-1, 0, 0], [0, 1, 0], [0, -1, 0]]
+offset_directions = [[1, 0, 0]]
+num_offsets = len(offset_directions)
+primitives = Manipulation.primitives
+num_primitives = len(primitives)
 
-
-total_start = time.perf_counter()
+solve_first = []
+solve_incre = []
+simulate_times = []
+disk_io_times = []
 
 config = PandaScenario()
-config.add_boxes_to_scene(seed=SEED)
-camera_positions = config.camera_positions
+config.add_boxes(density=500, seed=SEED)
 images, depths, seg_ids = config.compute_images_depths_and_seg_ids()
 
-with h5py.File(DATASET_PATH, "w") as f:
-    dp_group = f.create_group("datapoint_0001")
-    dp_group.attrs["seed"] = SEED
-    dp_group.create_dataset("camera_positions", data=camera_positions)
-    dp_group.create_dataset("images", data=images, compression="gzip", chunks=True)
-    dp_group.create_dataset("depths", data=depths, compression="gzip", chunks=True)
-    dp_group.create_dataset("seg_ids", data=seg_ids, compression="gzip", chunks=True)
+num_objects = len(config.man_frames)
 
-    objects_group = dp_group.create_group("objects")
+poses = np.zeros((num_objects, 7), dtype=np.float32)
+sizes = np.zeros((num_objects, 3), dtype=np.float32)
+target_poses = np.zeros((num_objects, num_offsets, 7), dtype=np.float32)
+feasibles = np.zeros((num_objects, num_offsets, num_primitives), dtype=np.int8)
+final_poses = np.zeros((num_objects, num_offsets, num_primitives, 7), dtype=np.float32)
 
-    solve_feasible_count = 0
-    solve_infeasible_count = 0
+for oi, obj in enumerate(config.man_frames):
+    frame = config.getFrame(obj)
+    poses[oi] = frame.getRelativePose()
+    sizes[oi] = frame.getSize()[:3]
 
-    for obj_idx, obj in enumerate(config.man_frames):
-        obj_start = time.perf_counter()
-        print(f"Processing object {obj_idx + 1}/{len(config.man_frames)}: {obj}")
+    for ti, direction in enumerate(offset_directions):
+        target_pos = config.sample_target_pos(obj, direction, seed=SEED)
+        target_pose = np.concatenate([target_pos, frame.getRelativeQuaternion()])
+        target_poses[oi][ti] = target_pose
 
-        obj_frame = config.getFrame(obj)
-        obj_group = objects_group.create_group(obj)
-        obj_group.create_dataset("pose", data=obj_frame.getRelativePose().astype(np.float32))
-        obj_group.create_dataset("size", data=obj_frame.getSize()[:3].astype(np.float32))
-
-        primitives_group = obj_group.create_group("primitives")
-
-        for prim_idx, primitive_name in enumerate(Manipulation.primitives):
-            prim_start = time.perf_counter()
-
-            _, primitive_dim, primitive_dir = primitive_name.split("_")
-            axis = {"x": 0, "y": 1, "z": 2}[primitive_dim]
-            direction = {"pos": 1, "neg": -1}[primitive_dir]
-            offset_local = np.zeros(3)
-            offset_local[axis] = POS_OFFSET * direction
-            offset_world = obj_frame.getRotationMatrix() @ offset_local
-            target_pos = obj_frame.getRelativePosition() + offset_world
-            target_pose = np.concatenate([target_pos, obj_frame.getRelativeQuaternion()])
-
+        for pi, primitive in enumerate(primitives):
             man = Manipulation(config, obj, slices=SLICES)
+            getattr(man, primitive)()
             man.target_pose(target_pose)
 
-            getattr(man, primitive_name)()
+            t0 = time.perf_counter()
+            feasible = man.solve().feasible
+            t1 = time.perf_counter()
+            dt = t1 - t0
+            solve_first.append(dt)
 
-            # Time solve() operation
-            solve_start = time.perf_counter()
-            ret = man.solve()
-            solve_duration = time.perf_counter() - solve_start
+            if feasible and INCREMENTAL_SLICES:
+                man = Manipulation(config, obj, slices=SLICES * 2)
+                getattr(man, primitive)()
+                man.target_pose(target_pose)
 
-            if ret.feasible:
-                timing_data["solve_feasible"].append(solve_duration)
-                solve_feasible_count += 1
+                t0 = time.perf_counter()
+                feasible = man.solve().feasible
+                t1 = time.perf_counter()
+                dt = t1 - t0
+                solve_incre.append(dt)
 
-                # Time simulate() operation
-                sim_start = time.perf_counter()
+            if feasible:
+                t0 = time.perf_counter()
                 man.simulate(view=False)
-                sim_duration = time.perf_counter() - sim_start
-                timing_data["simulate"].append(sim_duration)
-            else:
-                timing_data["solve_infeasible"].append(solve_duration)
-                solve_infeasible_count += 1
+                t1 = time.perf_counter()
+                simulate_times.append(t1 - t0)
 
-            final_pose = obj_frame.getRelativePose()
+            feasibles[oi][ti][pi] = feasible
+            final_poses[oi][ti][pi] = man.config.getFrame(obj).getRelativePose()
 
-            # Save results
-            prim_group = primitives_group.create_group(primitive_name)
-            prim_group.create_dataset("target_pose", data=target_pose.astype(np.float32))
-            prim_group.create_dataset("feasible", data=ret.feasible)
-            prim_group.create_dataset("final_pose", data=final_pose.astype(np.float32))
 
-            prim_duration = time.perf_counter() - prim_start
-            timing_data["per_primitive"][primitive_name].append(prim_duration)
+t0 = time.perf_counter()
+with h5py.File(DATASET_PATH, "w") as f:
+    f.attrs["primitives"] = np.array(primitives, dtype=h5py.string_dtype(encoding="utf-8"))
+    dp = f.create_group(f"dp_{SEED:04d}")
+    dp.create_dataset("cam_poses", data=config.cam_poses)
+    dp.create_dataset("images", data=images, compression="gzip", chunks=True)
+    dp.create_dataset("depths", data=depths, compression="gzip", chunks=True)
+    dp.create_dataset("seg_ids", data=seg_ids, compression="gzip", chunks=True)
+    dp.create_dataset("poses", data=poses)
+    dp.create_dataset("sizes", data=sizes)
+    dp.create_dataset("target_poses", data=target_poses)
+    dp.create_dataset("feasibles", data=feasibles)
+    dp.create_dataset("final_poses", data=final_poses)
+t1 = time.perf_counter()
+disk_io_times.append(t1 - t0)
 
-        timing_data["per_object"][obj] = time.perf_counter() - obj_start
-
-total_duration = time.perf_counter() - total_start
-print(f"Total pipeline duration: {total_duration:.2f}s")
-
-print("\nSOLVE() PERFORMANCE:")
-if timing_data["solve_feasible"]:
-    feasible_times = np.array(timing_data["solve_feasible"])
-    print(f"Feasible solves: {len(feasible_times)}")
-    print(f"  Total time: {feasible_times.sum():.2f}s")
-    print(f"  Average: {feasible_times.mean():.3f}s")
-    print(f"  Min/Max: {feasible_times.min():.3f}s / {feasible_times.max():.3f}s")
-
-if timing_data["solve_infeasible"]:
-    infeasible_times = np.array(timing_data["solve_infeasible"])
-    print(f"Infeasible solves: {len(infeasible_times)}")
-    print(f"  Total time: {infeasible_times.sum():.2f}s")
-    print(f"  Average: {infeasible_times.mean():.3f}s")
-    print(f"  Min/Max: {infeasible_times.min():.3f}s / {infeasible_times.max():.3f}s")
-
-print("\nSIMULATE() PERFORMANCE:")
-if timing_data["simulate"]:
-    sim_times = np.array(timing_data["simulate"])
-    print(f"Simulate calls: {len(sim_times)}")
-    print(f"  Total time: {sim_times.sum():.2f}s")
-    print(f"  Average: {sim_times.mean():.3f}s")
-    print(f"  Min/Max: {sim_times.min():.3f}s / {sim_times.max():.3f}s")
-
-print("\nPER-PRIMITIVE PERFORMANCE:")
-for primitive_name, times in timing_data["per_primitive"].items():
-    times_arr = np.array(times)
-    print(f"{primitive_name}: {times_arr.mean():.3f}s avg ({len(times)} calls)")
-
-print("\nPER-OBJECT PERFORMANCE:")
-for obj_name, duration in timing_data["per_object"].items():
-    print(f"{obj_name}: {duration:.2f}s")
-
-total_solve_time = sum(timing_data["solve_feasible"]) + sum(timing_data["solve_infeasible"])
-total_sim_time = sum(timing_data["simulate"])
-print("\nSOLVE vs SIMULATE COMPARISON:")
-print(f"Total solve() time: {total_solve_time:.2f}s")
-print(f"Total simulate() time: {total_sim_time:.2f}s")
+print("\n=== RESULTS ===")
+print(f"solve() first calls: {len(solve_first)}, avg={np.mean(solve_first):.4f}s, total={np.sum(solve_first):.2f}s")
+print(f"solve() incre calls: {len(solve_incre)}, avg={np.mean(solve_incre) if solve_incre else 0:.4f}s, total={np.sum(solve_incre):.2f}s")
+print(f"simulate() calls:    {len(simulate_times)}, avg={np.mean(simulate_times)}")
+print(f"Simulate time: avg={np.mean(simulate_times):.4f}s, total={np.sum(simulate_times):.2f}s")
+print(f"Disk I/O time (group & datasets): {np.sum(disk_io_times):.4f}s")
